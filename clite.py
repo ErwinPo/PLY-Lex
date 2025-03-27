e64d7b05bb69b77de107c87d592dfaf9bd0717ad
ErwinPo <A01734881@tec.mx>
2025-03-27 17:44:44 -0600

    First float test complete Hugo commit

--- clite.py
+++ clite.py
@@ -1,12 +1,19 @@
 import ply.lex as lex
 
-tokens = ['INT']
+tokens = ['FLOAT', 'INT']
 
 t_ignore = ' \t'
 
+# Define el reconocimiento de números flotantes básicos
+def t_FLOAT(t):
+    r'(\d+\.\d*|\.\d+|\d+\.)'
+    t.value = float(t.value)
+    return t
+
+# Define el reconocimiento de números enteros
 def t_INT(t):
-    r'\d+(_\d)*'
-    t.value = int(t.value)
+    r'\d+(_\d+)*'
+    t.value = int(t.value.replace('_', ''))
     return t
 
 def getLexer():
