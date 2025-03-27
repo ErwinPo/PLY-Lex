a0ea91b03fe6b2d23f2eefa9d6ee70811157b73b
ErwinPo <A01734881@tec.mx>
2025-03-27 17:14:59 -0600

    fisrt test complete

--- clite.py
+++ clite.py
@@ -1,9 +1,13 @@
 import ply.lex as lex
 
-tokens = [ 'INT' ]
+tokens = ['INT']
 
-t_ignore  = ' \t'
+t_ignore = ' \t'
 
+def t_INT(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
 
 def getLexer():
-  return lex.lex()
+    return lex.lex()
