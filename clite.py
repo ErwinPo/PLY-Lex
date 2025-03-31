6338d8a423b41a8c160a477733c4b2050cf095f4
unknown <hugomr123@outlook.com>
2025-03-31 15:36:17 -0600

    first string test complete, committed by Hugo

--- clite.py
+++ clite.py
@@ -1,18 +1,22 @@
 import ply.lex as lex
 
-tokens = ['FLOAT', 'INT']
+tokens = ['INT', 'FLOAT', 'STR']
 
 t_ignore = ' \t'
+
+def t_INT(t):
+    r'\d+(_\d+)*'
+    t.value = int(t.value.replace('_', ''))
+    return t
 
 def t_FLOAT(t):
     r'(\d+(_\d+)*(\.\d*)?|\.\d+(_\d+)*)([eE][+-]?\d+(_\d+)*)?'
     t.value = float(t.value.replace('_', ''))
     return t
 
-
-def t_INT(t):
-    r'\d+(_\d+)*'
-    t.value = int(t.value.replace('_', ''))
+def t_STR(t):
+    r'"(?:\\.|[^"\\])*"'
+    t.value = t.value[1:-1]  
     return t
 
 def getLexer():
